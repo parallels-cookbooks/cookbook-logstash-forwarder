8a4863b4f7281b2d72677fa23adcd0e9e150a839
Kirill Kouznetsov <agon.smith@gmail.com>
2015-06-10 21:08:22 +0300

    Heavy-Weight Resources/Providers were switched to LWRPs

---
+++ resources/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: logstash-forwarder
+# Resource:: log_forward
+# Author:: Kirill Kouznetsov <agon.smith@gmail.com>
+#
+# Copyright (c) 2015, Parallels IP Holdings GmbH
+#
+
+provides :log_forward
+resource_name :log_forward
+actions :create
+default_action :create
+
+attribute :paths, kind_of: Array, required: true
+attribute :fields, kind_of: Hash, default: {}, required: true
