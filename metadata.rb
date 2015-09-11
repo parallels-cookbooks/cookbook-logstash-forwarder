d332b83183de19714f00c2c841b46fa4dd076cfa
Pavel Yudin <pyudin@parallels.com>
2015-09-11 14:24:58 +0300

    Version bumped

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pyudin@parallels.com'
 license          'Apache 2.0'
 description      'Installs and configures logstash-forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 supports 'amazon'
 supports 'redhat'
